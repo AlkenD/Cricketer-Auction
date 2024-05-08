4c1297b74cf599b55e9ebfb3f4236ef2c893a76b
AlkenD <desteralken@gmail.com>
2024-05-08 13:25:37 +0530

    feat: fullscreen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	export let data;
+	import Fullscreen from 'svelte-fullscreen';
+
 	let currentPlayer: number = 0;
-	let loading = false;
 
 	const handlePrevious = () => {
 		if (currentPlayer > 0) {
@@ -21,151 +22,191 @@
 	$: currentSet = data.ipl.filter((obj) => obj.set === currentSetNumber) || [];
 </script>
 
-<div
-	class="p-20 h-screen flex justify-center items-center bg-gradient-to-tr from-indigo-600 to-yellow-600 font-serif relative"
->
-	<div class="absolute top-2 left-2 space-x-2 flex z-50">
+<Fullscreen let:onRequest let:onExit>
+	<div
+		class="p-20 h-screen flex justify-center items-center bg-gradient-to-tr from-indigo-600 to-yellow-600 font-serif relative"
+	>
+		<div class="absolute top-2 left-2 space-x-2 flex z-50">
+			{#if currentSet.length !== 0}
+				<div class="px-4 py-2 font-bold bg-white rounded-full">
+					Player - {currentPlayer + 1} / {currentSet.length}
+				</div>
+			{:else}
+				<div class="px-4 py-2 font-bold bg-white rounded-full">
+					Player - {currentPlayer + 1} / {data.ipl.length}
+				</div>
+			{/if}
+
+			<select
+				bind:value={currentSetNumber}
+				on:change={() => (currentPlayer = 0)}
+				class="px-4 py-2 font-bold bg-white rounded-full"
+			>
+				<option value="0">All Players</option>
+				{#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] as item}
+					<option value={`${item}`}>Set {item}</option>
+				{/each}
+			</select>
+		</div>
 		{#if currentSet.length !== 0}
-			<div class="px-4 py-2 font-bold bg-white rounded-full">
-				Player - {currentPlayer + 1} / {currentSet.length}
+			<div class="membr-details flex justify-center items-center">
+				<div class="membr-details-img relative">
+					<img src={currentSet[currentPlayer].image} alt="" />
+					<div
+						class="plyr-name-nationality absolute bottom-0 left-0 right-0 w-full h-fit z-30 text-center backdrop-blur-2xl p-6"
+					>
+						<h1 class="text-4xl font-bold text-white pb-8">{currentSet[currentPlayer].name}</h1>
+						<span class="text-2xl">{currentSet[currentPlayer].country}</span>
+					</div>
+				</div>
+				<div class="player-overview-detail space-y-4">
+					<h1 class="text-2xl font-bold">Player Overview</h1>
+					<div class="grid grid-cols-2 text-center">
+						<div
+							class="p-4 border-l border-t border-r border-black flex flex-col justify-center items-center backdrop-blur-2xl"
+						>
+							<p class="text-xl font-bold">{currentSet[currentPlayer].type}</p>
+							<span>Specialization</span>
+						</div>
+						<div
+							class="p-4 border-t border-r bg-white text-black border-black flex flex-col justify-center items-center"
+						>
+							<p class="text-xl font-bold">
+								{currentSet[currentPlayer].basevalue} Cr.
+							</p>
+							<span>Base Price</span>
+						</div>
+						<div
+							class="p-4 border-t border-l border-r border-b border-black flex flex-col justify-center items-center"
+						>
+							<p class="text-xl font-bold">{currentSet[currentPlayer].ratingHidden}</p>
+							<span>Rating</span>
+						</div>
+						<div
+							class="p-4 border-r border-b border-t border-black flex flex-col justify-center items-center"
+						>
+							<p class="text-xl font-bold">{currentSet[currentPlayer].fanstarsHidden}</p>
+							<span>Fan Rating</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		{:else}
-			<div class="px-4 py-2 font-bold bg-white rounded-full">
-				Player - {currentPlayer + 1} / {data.ipl.length}
+			<div class="membr-details flex justify-center items-center">
+				<div class="membr-details-img relative">
+					<img src={data.ipl[currentPlayer].image} alt="" />
+					<div
+						class="plyr-name-nationality absolute bottom-0 left-0 right-0 w-full h-fit z-30 text-center backdrop-blur-2xl p-6"
+					>
+						<h1 class="text-4xl font-bold text-white pb-8">{data.ipl[currentPlayer].name}</h1>
+						<span class="text-2xl">{data.ipl[currentPlayer].country}</span>
+					</div>
+				</div>
+				<div class="player-overview-detail space-y-4">
+					<h1 class="text-2xl font-bold">Player Overview</h1>
+					<div class="grid grid-cols-2 text-center">
+						<div
+							class="p-4 border-l border-t border-r border-black flex flex-col justify-center items-center"
+						>
+							<p class="text-xl font-bold">{data.ipl[currentPlayer].type}</p>
+							<span>Specialization</span>
+						</div>
+						<div
+							class="p-4 border-t border-r bg-white text-black border-black flex flex-col justify-center items-center"
+						>
+							<p class="text-xl font-bold">
+								{data.ipl[currentPlayer].basevalue} Cr.
+							</p>
+							<span>Base Price</span>
+						</div>
+						<div
+							class="p-4 border-t border-l border-r border-b border-black flex flex-col justify-center items-center"
+						>
+							<p class="text-xl font-bold">{data.ipl[currentPlayer].ratingHidden}</p>
+							<span>Rating</span>
+						</div>
+						<div
+							class="p-4 border-r border-b border-t border-black flex flex-col justify-center items-center"
+						>
+							<p class="text-xl font-bold">{data.ipl[currentPlayer].fanstarsHidden}</p>
+							<span>Fan Rating</span>
+						</div>
+					</div>
+				</div>
 			</div>
 		{/if}
-
-		<select
-			bind:value={currentSetNumber}
-			on:change={() => (currentPlayer = 0)}
-			class="px-4 py-2 font-bold bg-white rounded-full"
-		>
-			<option value="0">All Players</option>
-			{#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] as item}
-				<option value={`${item}`}>Set {item}</option>
-			{/each}
-		</select>
-	</div>
-	{#if currentSet.length !== 0}
-		<div class="membr-details flex justify-center items-center">
-			<div class="membr-details-img relative">
-				<img src={currentSet[currentPlayer].image} alt="" />
-				<div
-					class="plyr-name-nationality absolute bottom-0 left-0 right-0 w-full h-fit z-30 text-center backdrop-blur-2xl p-6"
-				>
-					<h1 class="text-4xl font-bold text-white pb-8">{currentSet[currentPlayer].name}</h1>
-					<span class="text-2xl">{currentSet[currentPlayer].country}</span>
-				</div>
-			</div>
-			<div class="player-overview-detail space-y-4">
-				<h1 class="text-2xl font-bold">Player Overview</h1>
-				<div class="grid grid-cols-2 text-center">
-					<div
-						class="p-4 border-l border-t border-r border-black flex flex-col justify-center items-center backdrop-blur-2xl"
-					>
-						<p class="text-xl font-bold">{currentSet[currentPlayer].type}</p>
-						<span>Specialization</span>
-					</div>
-					<div
-						class="p-4 border-t border-r bg-white text-black border-black flex flex-col justify-center items-center"
-					>
-						<p class="text-xl font-bold">
-							{currentSet[currentPlayer].basevalue} Cr.
-						</p>
-						<span>Base Price</span>
-					</div>
-					<div
-						class="p-4 border-t border-l border-r border-b border-black flex flex-col justify-center items-center"
-					>
-						<p class="text-xl font-bold">{currentSet[currentPlayer].ratingHidden}</p>
-						<span>Rating</span>
-					</div>
-					<div
-						class="p-4 border-r border-b border-t border-black flex flex-col justify-center items-center"
-					>
-						<p class="text-xl font-bold">{currentSet[currentPlayer].fanstarsHidden}</p>
-						<span>Fan Rating</span>
-					</div>
-				</div>
-			</div>
+		<div class="absolute bottom-2 right-2">
+			<button class="p-2 rounded-full bg-white text-black" on:click={() => onRequest()}
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="icon icon-tabler icons-tabler-outline icon-tabler-arrows-maximize"
+					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M16 4l4 0l0 4" /><path
+						d="M14 10l6 -6"
+					/><path d="M8 20l-4 0l0 -4" /><path d="M4 20l6 -6" /><path d="M16 20l4 0l0 -4" /><path
+						d="M14 14l6 6"
+					/><path d="M8 4l-4 0l0 4" /><path d="M4 4l6 6" /></svg
+				></button
+			>
+			<button class="p-2 rounded-full bg-white text-black" on:click={() => onExit()}
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="icon icon-tabler icons-tabler-outline icon-tabler-arrows-minimize"
+					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 9l4 0l0 -4" /><path
+						d="M3 3l6 6"
+					/><path d="M5 15l4 0l0 4" /><path d="M3 21l6 -6" /><path d="M19 9l-4 0l0 -4" /><path
+						d="M15 9l6 -6"
+					/><path d="M19 15l-4 0l0 4" /><path d="M15 15l6 6" /></svg
+				></button
+			>
+			<button class="p-2 rounded-full bg-white text-black" on:click={handlePrevious}
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"
+					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 6l-6 6l6 6" /></svg
+				></button
+			>
+			<button class="p-2 rounded-full bg-white text-black" on:click={handleNext}
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"
+					><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 6l6 6l-6 6" /></svg
+				></button
+			>
 		</div>
-	{:else}
-		<div class="membr-details flex justify-center items-center">
-			<div class="membr-details-img relative">
-				<img src={data.ipl[currentPlayer].image} alt="" />
-				<div
-					class="plyr-name-nationality absolute bottom-0 left-0 right-0 w-full h-fit z-30 text-center backdrop-blur-2xl p-6"
-				>
-					<h1 class="text-4xl font-bold text-white pb-8">{data.ipl[currentPlayer].name}</h1>
-					<span class="text-2xl">{data.ipl[currentPlayer].country}</span>
-				</div>
-			</div>
-			<div class="player-overview-detail space-y-4">
-				<h1 class="text-2xl font-bold">Player Overview</h1>
-				<div class="grid grid-cols-2 text-center">
-					<div
-						class="p-4 border-l border-t border-r border-black flex flex-col justify-center items-center"
-					>
-						<p class="text-xl font-bold">{data.ipl[currentPlayer].type}</p>
-						<span>Specialization</span>
-					</div>
-					<div
-						class="p-4 border-t border-r bg-white text-black border-black flex flex-col justify-center items-center"
-					>
-						<p class="text-xl font-bold">
-							{data.ipl[currentPlayer].basevalue} Cr.
-						</p>
-						<span>Base Price</span>
-					</div>
-					<div
-						class="p-4 border-t border-l border-r border-b border-black flex flex-col justify-center items-center"
-					>
-						<p class="text-xl font-bold">{data.ipl[currentPlayer].ratingHidden}</p>
-						<span>Rating</span>
-					</div>
-					<div
-						class="p-4 border-r border-b border-t border-black flex flex-col justify-center items-center"
-					>
-						<p class="text-xl font-bold">{data.ipl[currentPlayer].fanstarsHidden}</p>
-						<span>Fan Rating</span>
-					</div>
-				</div>
-			</div>
-		</div>
-	{/if}
-	<div class="absolute bottom-2 right-2">
-		<button class="p-2 rounded-full bg-white text-black" on:click={handlePrevious}
-			><svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"
-				><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 6l-6 6l6 6" /></svg
-			></button
-		>
-		<button class="p-2 rounded-full bg-white text-black" on:click={handleNext}
-			><svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-right"
-				><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 6l6 6l-6 6" /></svg
-			></button
-		>
 	</div>
-</div>
+</Fullscreen>
 
 <style>
 	:global(.svelte-image-loader) {
